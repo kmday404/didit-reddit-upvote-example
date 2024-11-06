0c41595aed43605f0b2f482a20ddc57dcb81354b
Katherine Day <kmday125@gmail.com>
2024-11-06 14:28:04 +0000

    error page update

--- src/app/error.js
+++ src/app/error.js
@@ -3,16 +3,14 @@ import Link from "next/link";
 
 export default function Error({ error, reset }) {
   return (
-    <html>
-      <body>
-        <h2>Oh no! Something went wrong</h2>
+    <div>
+      <h2>Oh no! Something went wrong</h2>
 
-        <p>{error.message}</p>
-        <button onClick={() => reset()}>Try again</button>
-        <br />
-        <Link href="/">Go Home</Link>
-      </body>
-    </html>
+      <p>{error.message}</p>
+      <button onClick={() => reset()}>Try again</button>
+      <br />
+      <Link href="/">Go Home</Link>
+    </div>
   );
 }
 
