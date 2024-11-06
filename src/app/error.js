b9d489dd16c18853bf38160b2114d88246c6764b
Katherine Day <kmday125@gmail.com>
2024-11-06 14:31:24 +0000

    test

--- src/app/error.js
+++ src/app/error.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 
-export default function Error({ error, reset }) {
+export default function GlobalError({ error, reset }) {
   return (
     <div>
       <h2>Oh no! Something went wrong</h2>
