3461d256eda83b7c012228bbe025abbf4e3cdf63
Katherine Day <kmday125@gmail.com>
2024-11-06 14:29:13 +0000

    not found updated

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ export default function NotFoundPage() {
     <>
       <h1>NOT FOUND</h1>
       <h2>Are you lost?</h2>
-      <p>we'll lend you a hand to</p>
+      <p>We can help you</p>
       <Link href="/">Go Home</Link>
       {/* <Image
         src="https://images.unsplash.com/photo-1510133768164-a8f7e4d4e3dc?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
